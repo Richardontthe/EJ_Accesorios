f98436027a5088b70e1f7e020d16ff1915a89f9d
RICARDO HERNANDEZ SALAS <rhernandezsalas24@gmail.com>
2026-03-29 22:26:38 -0600

    FAQ base

--- assets/navigation.js
+++ assets/navigation.js
@@ -2,6 +2,18 @@
 
 $(document).ready(function() {
 
- 
+ $(".inner-faq").hide();
+  
+    // click para que se muestre el contenido de cada bloque de la historia
+    $(".FAQ-Banner").click(function () {
+
+        $(this)
+            .next(".inner-faq")
+            .slideToggle(500);
+
+    });
+    
+
+
 
 });
