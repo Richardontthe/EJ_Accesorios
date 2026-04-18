0addc88d33742708e38b4ac86ddd7289e1c42d47
RICARDO HERNANDEZ SALAS <rhernandezsalas24@gmail.com>
2026-04-18 14:41:10 -0600

    agrega tiktok

--- assets/musica.js
+++ assets/musica.js
@@ -5,10 +5,44 @@ $(document).ready(function () {
 
     // Lista de artistas populares 
     const ARTISTS = [
-        "coldplay", "adele", "eminem", "taylor+swift", "the+beatles",
+         "coldplay", "adele", "eminem", "taylor+swift", "the+beatles",
         "daft+punk", "queen", "michael+jackson", "beyonce", "ed+sheeran",
         "bad+bunny", "shakira", "radiohead", "arctic+monkeys", "kanye+west",
-        "billie+eilish", "the+weeknd", "drake", "rihanna", "linkin+park"
+        "billie+eilish", "the+weeknd", "drake", "rihanna", "linkin+park",
+
+         "ariana+grande", "dua+lipa", "harry+styles", "post+malone",
+        "bruno+mars", "lady+gaga", "miley+cyrus", "sza", "olivia+rodrigo",
+        "sabrina+carpenter", "chappell+roan", "charlie+puth",
+        "sam+smith", "lana+del+rey", "selena+gomez",
+
+         "travis+scott", "kendrick+lamar", "j+cole", "21+savage",
+        "juice+wrld", "xxxtentacion", "lil+baby", "future",
+        "nicki+minaj", "cardi+b", "tyler+the+creator",
+
+         "frank+ocean", "usher", "the+dream", "h.e.r",
+        "daniel+caesar", "giveon", "benson+boone",
+
+         "david+guetta", "calvin+harris", "martin+garrix",
+        "marshmello", "alan+walker", "avicii", "tiesto",
+
+         "imagine+dragons", "red+hot+chili+peppers", "foo+fighters",
+        "nirvana", "green+day", "the+killers", "muse",
+        "twenty+one+pilots", "fall+out+boy", "panic+at+the+disco",
+        "system+of+a+down", "metallica",
+
+         "peso+pluma", "feid", "karol+g", "j+balvin", "ozuna",
+        "anuel+aa", "maluma", "nicky+jam", "daddy+yankee",
+        "rauw+alejandro", "jhay+cortez", "myke+towers",
+        "farruko", "sech", "noriel", "lunay",
+
+         "christian+nodal", "natanael+cano", "xavi",
+        "grupo+frontera", "eslabon+armado", "los+plebes+del+rancho",
+
+         "rosalia", "carlos+vives", "enrique+iglesias",
+        "marc+anthony", "juan+luis+guerra", "camilo",
+        "sebastian+yatra", "manuel+turizo", "reik", "ha+ash",
+
+         "bts", "blackpink", "stray+kids",
     ];
 
     const API_KEY = "123"; // esta gratis papa
